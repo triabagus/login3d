a8ee6b224bc35d3281fea780e355c98bf57bf730
triabagus <triatop9@gmail.com>
2023-01-06 20:45:03 +0700

    Show/Hide Password Js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPass = (inputPass, inputIcon) => {
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+    
+    iconEye.addEventListener('click', () =>{
+        // Change password to text
+        if(input.type === 'password'){
+            // Switch to text
+            input.type = 'text'
+            
+            // Add icon
+            iconEye.classList.add('ri-eye-line')
+            // Remove icon
+            iconEye.classList.remove('ri-eye-off-line')
+        }else{
+          // Change password
+          input.type = "password";
+
+          // Add icon
+          iconEye.classList.add("ri-eye-off-line");
+          // Remove icon
+          iconEye.classList.remove("ri-eye-line");
+        }
+    })
+}
+
+showHiddenPass('input-pass', 'input-icon')
